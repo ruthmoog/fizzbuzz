c52a71eec37b8857d2e3e8d95c8f158ded766bc8
Ruth Baker <ruthmoog@gmail.com>
2019-05-21 15:22:16 +0100

    Beautiful and elegant ternary operators

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -6,5 +6,5 @@ def fizzbuzz(num)
 end
 
 def calc_fizzbuzz(num)
-  !(num % FIZZNUM == 0 || num % BUZZNUM == 0) ? num.to_s : (num % FIZZNUM == 0 ? "fizz" : "buzz")
+  !(num % FIZZNUM == 0 || num % BUZZNUM == 0) ? num.to_s : (num % FIZZNUM == 0 && num % BUZZNUM == 0 ? "fizzbuzz" : (num % FIZZNUM == 0 ? "fizz" : "buzz"))
 end
